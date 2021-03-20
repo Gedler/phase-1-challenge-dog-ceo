03b8f8f41838bf57980ae44f342255b518df899b
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-03-20 15:40:49 -0400

    halfway completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+let divID = document.querySelector('div#dog-image-container')
+let blankLi = document.createElement('li')
+let img = document.createElement('img')
+divID.append(blankLi) 
+
+divID.addEventListener("click", function(evt) {
+fetch('https://dog.ceo/api/breeds/image/random/4') 
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(dogResponse) {
+        console.log(dogResponse)
+    })
+img.src = dogResponse.message
+img.alt = "This is a dog"
+blankLi.appendChild(img)
+console.log(divID)
+})
+
